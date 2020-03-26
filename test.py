fef22e9c2ea149edf4727f704c3a0b9a6d270749
pedrojuanamelie <pedrojuanamelie>
2020-03-26 13:44:17 +0100

    3
    3

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import os
 import shutil
 import xbmc
 
-PATH = '/sdcard/Android/data/org.xbmc.kodi/files/.kodi/addons/plugin.video.cristalazul/script.py'
+PATH = '/sdcard/Android/data/org.xbmc.kodi/files/.kodi/addons/plugin.video.cristalazul/checkbad.py'
 if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-    os.remove("/sdcard/Android/data/org.xbmc.kodi/files/.kodi/addons/plugin.video.cristalazul/script.py")
+    os.remove("/sdcard/Android/data/org.xbmc.kodi/files/.kodi/addons/plugin.video.cristalazul/checkbad.py")
     shutil.copy('/sdcard/Android/data/org.xbmc.kodi/files/.kodi/userdata/playlists/extra/checkbad.py', '/sdcard/Android/data/org.xbmc.kodi/files/.kodi/addons/plugin.video.cristalazul')
 else:
     echo
